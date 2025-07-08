ecc89f571417ac44545fa03f5840583271b44e6f
yock <167619555+yocksers@users.noreply.github.com>
2025-07-08 08:20:52 +0200

    Add files via upload

--- plugin.js
+++ plugin.js
@@ -1,7 +1,7 @@
 ﻿(function () {
     const pluginId = "b8d0f5a4-3e96-4c0f-a6e2-9f0c2ecb5c5f";
 
-    const pluginVersion = "2.8.10";
+    const pluginVersion = "2.9.0";
 
     window.Dashboard.getPluginPages = function () {
         return [
